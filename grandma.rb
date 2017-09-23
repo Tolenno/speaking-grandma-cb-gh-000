89a3551e1767e66cdd4c52f15ed907e2ac927329
Matt Hough <matt@hough.tv>
2017-09-23 22:53:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(speak)
-  if speak.upcase && speak != "I LOVE YOU GRANDMA!"
+  if speak.upcase == true && speak != "I LOVE YOU GRANDMA!"
     puts "NO, NOT SINCE 1938!"
   elsif speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
