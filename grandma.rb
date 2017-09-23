81985410766749ad8df2b0534eb0159fd5e8d4d7
Matt Hough <matt@hough.tv>
2017-09-23 23:06:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ require 'pry'
 def speak_to_grandma(speak)
   if speak == speak.upcase && speak != "I LOVE YOU GRANDMA!"
     puts "NO, NOT SINCE 1938!"
-  elsif speak.upcase == true && speak == "I LOVE YOU GRANDMA!"
+  elsif speak == speak.upcase && speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
