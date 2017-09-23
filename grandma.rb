084ad2c407f669b6f7af1531b014a0e363f027b4
Matt Hough <matt@hough.tv>
2017-09-23 22:52:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(speak = "I LOVE YOU GRANDMA!")
+def speak_to_grandma(speak)
   if speak.upcase && speak != "I LOVE YOU GRANDMA!"
     puts "NO, NOT SINCE 1938!"
   elsif speak == "I LOVE YOU GRANDMA!"
@@ -22,3 +22,5 @@ def speak_to_grandma(speak = "I LOVE YOU GRANDMA!")
   end
   binding.pry
 end
+
+speak_to_grandma
